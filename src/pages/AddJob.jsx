625d458363a162dead14ccf3c008da9d3a5ba128
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-12-21 00:34:29 -0500

    tanstack

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -2,13 +2,34 @@ import { useContext, useState } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import { AuthContext } from '../providers/AuthProvider'
-import axios from 'axios'
+// import axios from 'axios'
 import { toast } from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import useAxiosSecure from '../hooks/useAxiosSecure'
 const AddJob = () => {
+  const queryClient = useQueryClient(); //not mandatory
   const navigate = useNavigate()
+  const axiosSecure = useAxiosSecure();
   const { user } = useContext(AuthContext)
   const [startDate, setStartDate] = useState(new Date())
+
+  const { isPending, mutateAsync } = useMutation(
+    {
+      mutationFn: async jobData => {
+        // await axios.post(`${import.meta.env.VITE_API_URL}/add-job`, jobData)
+        await axiosSecure.post(`/add-job`, jobData)
+      },
+      onSuccess: () => {
+        console.log('data saved');
+        queryClient.invalidateQueries({ queryKey: ['jobs'] }) //not mandatory
+      },
+      onError: err => {
+        console.log(err);
+      }
+    }
+  );
+
   const handleSubmit = async e => {
     e.preventDefault()
     const form = e.target
@@ -36,7 +57,10 @@ const AddJob = () => {
     }
     try {
       // 1. make a post request
-      await axios.post(`${import.meta.env.VITE_API_URL}/add-job`, formData)
+      // await axios.post(`${import.meta.env.VITE_API_URL}/add-job`, formData)
+
+      // 1. Make a post request using useMutation hook
+      await mutateAsync(formData)
       // 2. Reset form
       form.reset()
       // 3. Show toast and navigate
@@ -143,7 +167,7 @@ const AddJob = () => {
           </div>
           <div className='flex justify-end mt-6'>
             <button className='disabled:cursor-not-allowed px-8 py-2.5 leading-5 text-white transition-colors duration-300 transhtmlForm bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600'>
-              Save
+              {isPending ? 'Saving...' : 'Save'}
             </button>
           </div>
         </form>
